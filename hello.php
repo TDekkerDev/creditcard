aad468acb57e4f2f006b8d66366ae2de5f38117b
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2021-09-22 15:03:58 +0200

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,7 +1,7 @@
 <?php
 $mysqli = new mysqli("localhost","root","","thomas");
 
-// Check connection
+
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
@@ -9,7 +9,7 @@ if ($mysqli -> connect_errno) {
 $creditcardnummer = $_POST['creditcardnummer'];
 $wachtword = $_POST['wachtword'];
 $expiry_date = $_POST['expiry_date'];
-// Perform query
+
 if ($mysqli->query("INSERT INTO creditcard VALUES ('$creditcardnummer', '$wachtword', '$expiry_date')") === TRUE) {
     echo "New record created successfully";
   } else {
